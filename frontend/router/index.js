4bc1e27b937b1e0ec9f75602505b12d5817f2979
tenriajeng <ilhamtenriajeng03@gmail.com>
2020-10-04 21:13:14 +0800

    new-project-frontend

---
+++ frontend/router/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Main from "../view/admin/layouts/Main";
+import UserTable from "../view/admin/user/UserTable";
+
+function Router(props) {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Main />
+      </Route>
+      <Route path="/admin/users">
+        <UserTable />
+      </Route>
+    </Switch>
+  );
+}
+
+export default Router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
-import "./App.css";
-import "antd/dist/antd.css";
-import { BrowserRouter } from "react-router-dom";
-// import Header from './components/Header';
-import Router from "./router";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App(props) {
+function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
