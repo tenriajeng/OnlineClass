cce29e6526b223cf7d8d54c2de97561f2fd644ce
tenriajeng <ilhamtenriajeng03@gmail.com>
2020-10-13 04:53:31 +0800

    testing-card

--- frontend/src/view/admin/tes.js
+++ frontend/src/view/admin/tes.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Input, Row, Col, Card } from "antd";
+import Avatar from "antd/lib/avatar/avatar";
+import Meta from "antd/lib/card/Meta";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from "@ant-design/icons";
 
 class Tes extends React.Component {
     render() {
@@ -9,23 +12,39 @@ class Tes extends React.Component {
             <div>
                 <Row>
                     <Col flex="auto" style={colStyle}>
-                        <Card title="Card title" bordered={true} style={cardStyle}>
-                            <Input placeholder="Basic usage" />
+                        <Card
+                            style={{ width: 320 }}
+                            cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                            actions={[<SettingOutlined key="setting" />, <EditOutlined key="edit" />, <EllipsisOutlined key="ellipsis" />]}
+                        >
+                            <Meta title="Card title" description="This is the description" />
                         </Card>
                     </Col>
                     <Col flex="auto" style={colStyle}>
-                        <Card title="Card title" bordered={true} style={cardStyle}>
-                            <Input placeholder="Basic usage" />
+                        <Card
+                            style={{ width: 320 }}
+                            cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                            actions={[<SettingOutlined key="setting" />, <EditOutlined key="edit" />, <EllipsisOutlined key="ellipsis" />]}
+                        >
+                            <Meta title="Card title" description="This is the description" />
                         </Card>
                     </Col>
                     <Col flex="auto" style={colStyle}>
-                        <Card title="Card title" bordered={true} style={cardStyle}>
-                            <Input placeholder="Basic usage" />
+                        <Card
+                            style={{ width: 320 }}
+                            cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                            actions={[<SettingOutlined key="setting" />, <EditOutlined key="edit" />, <EllipsisOutlined key="ellipsis" />]}
+                        >
+                            <Meta title="Card title" description="This is the description" />
                         </Card>
                     </Col>
                     <Col flex="auto" style={colStyle}>
-                        <Card title="Card title" bordered={true} style={cardStyle}>
-                            <Input placeholder="Basic usage" />
+                        <Card
+                            style={{ width: 320 }}
+                            cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                            actions={[<SettingOutlined key="setting" />, <EditOutlined key="edit" />, <EllipsisOutlined key="ellipsis" />]}
+                        >
+                            <Meta title="Card title" description="This is the description" />
                         </Card>
                     </Col>
                 </Row>
