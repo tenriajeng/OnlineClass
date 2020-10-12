e7ef03cbbde7042b4526b848fbc253e29f2200ff
tenriajeng <ilhamtenriajeng03@gmail.com>
2020-10-13 03:41:26 +0800

    code simplification

--- frontend/src/view/admin/layouts/Base.js
+++ frontend/src/view/admin/layouts/Base.js
@@ -1,81 +1,70 @@
 import { Breadcrumb, Layout, Menu } from "antd";
 import React from "react";
-import { UserOutlined, VideoCameraOutlined,BgColorsOutlined,DatabaseOutlined } from "@ant-design/icons";
-import { Link, Route } from "react-router-dom";
-import KelasTable from "../kelas/KelasTable";
-import UserTable from "../user/UserTable";
+import { UserOutlined, BgColorsOutlined, DatabaseOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 import SubMenu from "antd/lib/menu/SubMenu";
-import Tes from "../tes";
 import Router from "../../../router";
 
 const { Header, Content, Footer, Sider } = Layout;
 
 class Base extends React.Component {
-  state = {
-    collapsed: false,
-  };
+    state = {
+        collapsed: false,
+    };
 
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
 
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Sider
-            breakpoint="lg"
-            collapsedWidth="0"
-            onBreakpoint={(broken) => {
-              console.log(broken);
-            }}
-            onCollapse={(collapsed, type) => {
-              console.log(collapsed, type);
-            }}
-          >
-            <div style={{ margin: "24px 16px 13px" }} className="logo">
-              <h1 style={{ color: "white" }}>Admin Panel</h1>
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <Sider
+                        breakpoint="lg"
+                        collapsedWidth="0"
+                        onBreakpoint={(broken) => {
+                            console.log(broken);
+                        }}
+                        onCollapse={(collapsed, type) => {
+                            console.log(collapsed, type);
+                        }}
+                    >
+                        <div style={{ margin: "24px 16px 13px" }} className="logo">
+                            <h1 style={{ color: "white" }}>Admin Panel</h1>
+                        </div>
+                        <Menu theme="dark" mode="inline">
+                            <Menu.Item key="1" icon={<UserOutlined />}>
+                                <Link to="/admin/users">Users</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<DatabaseOutlined />}>
+                                <Link to="/admin/kelas">Kelas</Link>
+                            </Menu.Item>
+                            <Menu.Item key="3" icon={<BgColorsOutlined />}>
+                                <Link to="/admin/tes">Tes</Link>
+                            </Menu.Item>
+                            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                                <Menu.Item key="5">Tom</Menu.Item>
+                                <Menu.Item key="6">Bill</Menu.Item>
+                                <Menu.Item key="7">Alex</Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+                    <Layout>
+                        <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+                        <Content style={{ margin: "24px 16px 0" }}>
+                            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                                <Router />
+                            </div>
+                        </Content>
+                        <Footer style={{ textAlign: "center" }}>Ant Design ©2018 Created by Ant UED</Footer>
+                    </Layout>
+                </Layout>
             </div>
-            <Menu theme="dark" mode="inline">
-              <Menu.Item key="1" icon={<UserOutlined />}>
-                <Link to="/admin/users">Users</Link>
-              </Menu.Item>
-              <Menu.Item key="2" icon={<DatabaseOutlined />}>
-                <Link to="/admin/kelas">Kelas</Link>
-              </Menu.Item>
-              <Menu.Item key="3" icon={<BgColorsOutlined />}>
-                <Link to="/admin/tes">Tes</Link>
-              </Menu.Item>
-              <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-                <Menu.Item key="5">Tom</Menu.Item>
-                <Menu.Item key="6">Bill</Menu.Item>
-                <Menu.Item key="7">Alex</Menu.Item>
-              </SubMenu>
-            </Menu>
-          </Sider>
-          <Layout>
-            <Header
-              className="site-layout-sub-header-background"
-              style={{ padding: 0 }}
-            />
-            <Content style={{ margin: "24px 16px 0" }}>
-              <div
-                className="site-layout-background"
-                style={{ padding: 24, minHeight: 360 }}
-              >
-                <Router/>
-              </div>
-            </Content>
-            <Footer style={{ textAlign: "center" }}>
-              Ant Design ©2018 Created by Ant UED
-            </Footer>
-          </Layout>
-        </Layout>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Base;
--- frontend/src/view/admin/tes.js
+++ frontend/src/view/admin/tes.js
@@ -1,41 +1,37 @@
 import React from "react";
-import { Table, Space, Button, Modal, Input, Row, Col, Card } from "antd";
-
-const { Column } = Table;
+import { Input, Row, Col, Card } from "antd";
 
 class Tes extends React.Component {
-
-  render() {
-
-    const cardStyle ={marginRight:3,marginLeft:3};
-    const colStyle = {marginTop:10};
-    return (
-        <div>
-            <Row>
-                <Col flex="auto" style={colStyle}>
-                    <Card title="Card title" bordered={true} style={cardStyle}>
-                        <Input placeholder="Basic usage" />
-                    </Card>
-                </Col>
-                <Col flex="auto" style={colStyle}>
-                    <Card title="Card title" bordered={true} style={cardStyle}>
-                        <Input placeholder="Basic usage" />
-                    </Card>
-                </Col>
-                <Col flex="auto" style={colStyle}>
-                    <Card title="Card title" bordered={true} style={cardStyle}>
-                        <Input placeholder="Basic usage" />
-                    </Card>
-                </Col>
-                <Col flex="auto" style={colStyle}>
-                    <Card title="Card title" bordered={true} style={cardStyle}>
-                        <Input placeholder="Basic usage" />
-                    </Card>
-                </Col>
-            </Row>
-        </div>
-    );
-  }
+    render() {
+        const cardStyle = { marginRight: 3, marginLeft: 3 };
+        const colStyle = { marginTop: 10 };
+        return (
+            <div>
+                <Row>
+                    <Col flex="auto" style={colStyle}>
+                        <Card title="Card title" bordered={true} style={cardStyle}>
+                            <Input placeholder="Basic usage" />
+                        </Card>
+                    </Col>
+                    <Col flex="auto" style={colStyle}>
+                        <Card title="Card title" bordered={true} style={cardStyle}>
+                            <Input placeholder="Basic usage" />
+                        </Card>
+                    </Col>
+                    <Col flex="auto" style={colStyle}>
+                        <Card title="Card title" bordered={true} style={cardStyle}>
+                            <Input placeholder="Basic usage" />
+                        </Card>
+                    </Col>
+                    <Col flex="auto" style={colStyle}>
+                        <Card title="Card title" bordered={true} style={cardStyle}>
+                            <Input placeholder="Basic usage" />
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default Tes;
