35b39956d2229888a374145f85d5b3ff1106e446
tenriajeng <ilhamtenriajeng03@gmail.com>
2020-10-13 03:16:04 +0800

    route-fixed

--- backend/api/Routes/index.js
+++ backend/api/Routes/index.js
@@ -7,15 +7,17 @@ const materi = require("./admin/materis");
 const pemateri = require("./admin/pemateris");
 const Router = express.Router();
 
+
+Router.use("/admin/kelas", kelas);
+Router.use("/admin/user", user);
+Router.use("/admin/login",Login);
+Router.use("/admin/materi", materi);
+Router.use("/admin/pemateri", pemateri);
+
+
+Router.use("/home", guest);
 Router.use("/kelas", kelas);
 Router.use("/user", user);
 Router.use("/login", Login);
-Router.use("/guest", guest);
-Router.use("/admin/kelas", kelas);
-Router.use("/admin/user", user);
-Router.use("/admin/Login",Login);
-Router.use("/home", guest);
-Router.use("/admin/materi", materi);
-Router.use("/admin/pemateri", pemateri);
 
 module.exports = Router;
