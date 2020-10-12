4d2eb245dc29a81b86305b6ba7808f0f49205db6
Anong23 <anonglsanisa205@gmail.com>
2020-10-13 03:06:04 +0800

    Pemateri-Materi-added

---
+++ backend/api/Controllers/MaterisController.js
@@ -0,0 +1,56 @@
+"use strict";
+const materiModel = require("../Models/admin/Materis");
+const formRes = require("../Helpers/formRes");
+
+module.exports = {
+  getAllMateri: (req, res) => {
+    // const bookGenre = req.query.genre
+    materiModel
+      .showAllMateri()
+      .then((response) => formRes.resUser(res, response, 200))
+      .catch((err) => formRes.resUser(res, err, 404));
+  },
+  addMateri: (req, res) => {
+    //  const bodyReq = req.body;
+    var date = new Date();
+    const body = {
+      ...req.body,
+      created_at: date,
+      updated_at: date,
+    };
+    // console.log(body)
+    materiModel
+      .addMateri(body)
+      .then((response) => formRes.resUser(res, response, 200))
+      .catch((err) => formRes.resUser(res, err, 404));
+  },
+  updateMateri: (req, res) => {
+    var date = new Date();
+    const id = req.params.id;
+
+    // console.log('ini adalah id:',id)
+    const body = {
+      ...req.body,
+      updated_at: date,
+    };
+    // console.log(body)
+    materiModel
+      .updateMateri(body, id)
+      .then((response) => formRes.resUser(res, response, 200))
+      .catch((err) => formRes.resUser(res, err, 404));
+  },
+  deleteMateri: (req, res) => {
+    var date = new Date();
+    const id = req.params.id;
+    // console.log('ini adalah id:',id)
+    const body = {
+      updated_at: date,
+      deleted_at: date,
+    };
+    // console.log(body)
+    materiModel
+      .deleteMateri(body, id)
+      .then((response) => formRes.resUser(res, response, 200))
+      .catch((err) => formRes.resUser(res, err, 404));
+  },
+};
--- backend/api/Routes/index.js
+++ backend/api/Routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const kelas = require("./admin/kelas");
 const user = require("./admin/users");
+const materi = require("./admin/materis");
+const pemateri = require("./admin/pemateris");
 const Router = express.Router();
 
 Router.use("/admin/kelas", kelas);
 Router.use("/admin/user", user);
+Router.use("/admin/materi", materi);
+Router.use("/admin/pemateri", pemateri);
 
 module.exports = Router;
