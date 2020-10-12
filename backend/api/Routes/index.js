cb688ceba678af16a59a2cbccd97556ec0130e75
Anong23 <anonglsanisa205@gmail.com>
2020-10-13 04:51:11 +0800

    kelas-user

--- backend/api/Routes/index.js
+++ backend/api/Routes/index.js
@@ -3,11 +3,13 @@ const kelas = require("./admin/kelas");
 const user = require("./admin/users");
 const materi = require("./admin/materis");
 const pemateri = require("./admin/pemateris");
+const KelasUser = require("./admin/KelasUsers");
 const Router = express.Router();
 
 Router.use("/admin/kelas", kelas);
 Router.use("/admin/user", user);
 Router.use("/admin/materi", materi);
 Router.use("/admin/pemateri", pemateri);
+Router.use("/admin/kelas-user", KelasUser);
 
 module.exports = Router;
