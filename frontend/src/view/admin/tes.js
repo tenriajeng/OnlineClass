3bac730232d4d991e4ea0ca1246d4b8bf341b878
tenriajeng <ilhamtenriajeng03@gmail.com>
2020-10-14 02:43:07 +0800

    added-componet-tes

--- frontend/src/view/admin/tes.js
+++ frontend/src/view/admin/tes.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { Input, Row, Col, Card } from "antd";
-import Avatar from "antd/lib/avatar/avatar";
+import { Input, Checkbox, Row, Col, Card, DatePicker } from "antd";
 import Meta from "antd/lib/card/Meta";
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from "@ant-design/icons";
+import moment from "moment";
 
 class Tes extends React.Component {
     render() {
         const style = { background: "#0092ff", padding: "8px 0" };
         const cardStyle = { marginRight: 3, marginLeft: 3 };
         const colStyle = { marginTop: 10 };
+
+        function disabledDate(current) {
+            // Can not select days before today and today
+            return current && current < moment().endOf("day");
+        }
+
+        function onChange(e) {
+            console.log(`checked = ${e.target.checked}`);
+        }
+
         return (
             <div>
                 <Row justify="right">
@@ -51,6 +61,13 @@ class Tes extends React.Component {
                         >
                             <Meta title="Card title" description="This is the description" />
                         </Card>
+                    </Col>
+                </Row>
+
+                <Row justify="right" style={{ marginTop: 10 }}>
+                    <Col flex="1 1 240px" style={colStyle}>
+                        <Checkbox onChange={onChange}>Checkbox</Checkbox>
+                        <DatePicker format="YYYY-MM-DD HH:mm:ss" disabledDate={disabledDate} showTime={{ defaultValue: moment("00:00:00", "HH:mm:ss") }} />
                     </Col>
                 </Row>
             </div>
