4018ed4dce874c11eb6878236548dc8cd47daeb2
tenriajeng <ilhamtenriajeng03@gmail.com>
2020-10-13 21:30:51 +0800

    user-route-backend-fixed

--- backend/api/Routes/admin/users.js
+++ backend/api/Routes/admin/users.js
@@ -7,6 +7,5 @@ Router.get("/detail/:id", userController.getOneUser);
 Router.post("/create", userController.addUser);
 Router.put("/:id", userController.updateUser);
 Router.put("/delete/:id", userController.deleteUser);
-Router.get("/update", userController.getOneUser);
 
 module.exports = Router;
