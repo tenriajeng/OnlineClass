c260110670ed51f94a2eb4e104420f3c08b4a2f9
aderyan18 <marcryan61@gmail.com>
2020-10-13 21:33:35 +0800

    rute-fixed

--- backend/api/Routes/index.js
+++ backend/api/Routes/index.js
@@ -2,16 +2,12 @@ const express = require("express");
 const kelas = require("./admin/kelas");
 const user = require("./admin/users");
 const Login = require("./admin/login");
-const guest = require("./user/home.js")
+const home = require("./user/home.js")
 const Router = express.Router();
 
-Router.use("/kelas", kelas);
-Router.use("/user", user);
-Router.use("/login", Login);
-Router.use("/guest", guest);
 Router.use("/admin/kelas", kelas);
 Router.use("/admin/user", user);
 Router.use("/admin/Login",Login);
-Router.use("/home", guest);
+Router.use("/home", home);
 
 module.exports = Router;
