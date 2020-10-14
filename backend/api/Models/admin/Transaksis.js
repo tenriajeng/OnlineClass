9575320f1e8a655eabbd58b293ddcedc68d733c3
Anong23 <anonglsanisa205@gmail.com>
2020-10-15 05:39:49 +0800

    transaksi

--- backend/api/Models/admin/Transaksis.js
+++ backend/api/Models/admin/Transaksis.js
@@ -56,4 +56,18 @@ module.exports = {
       );
     });
   },
+  showOneTransaksi: (id) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT * FROM transaksis WHERE deleted_at IS NULL AND id = ${id} ORDER BY updated_at DESC`,
+        (err, response) => {
+          if (!err) {
+            resolve(response);
+          } else {
+            reject(err);
+          }
+        }
+      );
+    });
+  },
 };
--- backend/api/Routes/admin/Transaksis.js
+++ backend/api/Routes/admin/Transaksis.js
@@ -7,5 +7,6 @@ Router.get("/", transaksiController.getAllTransaksi);
 Router.post("/create", transaksiController.addTransaksi);
 Router.put("/:id", transaksiController.updateTransaksi);
 Router.put("/delete/:id", transaksiController.deleteTransaksi);
+Router.get("/detail/:id", transaksiController.getOneTransaksi);
 
 module.exports = Router;
