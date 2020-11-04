d38f173b216c70950a22888c3e9f8b4fa32fecf8
tenriajeng <ilhamtenriajeng03@gmail.com>
2020-11-04 13:02:01 +0800

    delete-folder

--- backend/api/Routes/index.js
+++ backend/api/Routes/index.js
@@ -7,24 +7,21 @@ const materi = require("./admin/materis");
 const pemateri = require("./admin/pemateris");
 const KelasUser = require("./admin/KelasUsers");
 const transaksi = require("./admin/Transaksis");
+const pembayaran = require("./admin/Pembayarans");
 
 //home
 const home = require("./user/home.js");
-const Login = require("./user/login");
-const Register = require("./user/register");
 const userkelas = require("./user/indexuser.js");
-const pembayaran = require("./admin/Pembayarans");
-const profile = require("./user/profile");
 const Transaksi = require("./user/transaksi");
 const MoreMateri = require("./user/moremateri");
-const userkelas = require("./user/indexuser");
+
 //user
 const Register = require("./user/register");
 const profile = require("./user/profile");
 const ReadMore = require("./user/readmore");
 const Login = require("./user/login");
 
-// admin
+// admin route
 Router.use("/admin/kelas", kelas);
 Router.use("/admin/user", user);
 Router.use("/admin/materi", materi);
@@ -33,7 +30,7 @@ Router.use("/admin/pembayaran", pembayaran);
 Router.use("/admin/transaksis", transaksi);
 Router.use("/admin/kelas-user", KelasUser);
 
-// home
+// home route
 Router.use("/home", home);
 Router.use("/login", Login);
 Router.use("/register", Register);
