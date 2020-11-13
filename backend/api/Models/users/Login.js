74fbbe78fe6310938fd9d67601aa51745f0171a8
clara <clarafalisha13@gmail.com>
2020-11-13 23:49:13 +0800

    jwt
    update jwt

--- backend/api/Models/users/Login.js
+++ backend/api/Models/users/Login.js
@@ -1,7 +1,19 @@
 const db = require("../../../config/db.js");
 
 module.exports = {
-    loginUser: (body) => {
+    loginUser: (email) => {
+        return new Promise((resolve, reject) => {
+            const query = `SELECT * FROM users WHERE email = ?  `;
+            db.query(query, [email], (err, result) => {
+                if (!err) {
+                    resolve(result);
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    },
+    /*
         return new Promise((resolve, reject) => {
             db.query(
                 "SELECT * FROM users WHERE email= ? AND password= ? AND deleted_at IS NULL",
@@ -16,4 +28,5 @@ module.exports = {
             );
         });
     },
+    */
 };
