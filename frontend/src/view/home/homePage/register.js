3cb2b605dd956849b6b095479b23b1bfdc3f9fd7
basosamriadi <72317222+basosamriadi@users.noreply.github.com>
2020-11-20 09:52:46 +0800

    update register

--- frontend/src/view/home/homePage/register.js
+++ frontend/src/view/home/homePage/register.js
@@ -2,48 +2,46 @@ import React from "react";
 import { Form, Input, Button, Row, Col } from "antd";
 import Axios from "axios";
 
-const onFinish = (values) => {
-    console.log("Received values of form: ", values);
-};
-
 class Register extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: [],
-            visible: false,
             name: "",
             email: "",
             password: "",
         };
+        // this.changeHandler = this.changeHandler.bind(this);
     }
 
-    onOk = () => {
-
+    changeHandler = (e) => {
         this.setState({
-            visibleUpdate: true,
-            name: data.name,
-            email: data.email,
-            password: data.password,
+            [e.target.name]: e.target.value,
         });
-        
-        cons data={
-            name=this.state.name,
-            email=this.state.email,
-            password=this.state.password,
-        }
+        // alert("handle : ", e.target.name);
+    };
 
+    submitHandler = (e) => {
+        alert("lol");
+        // e.preventDefault();
+        console.log("lol : ", this.state.name);
 
-        Axios.post("http://localhost:6600/register", data).then((res) => {
-            const data = res.data.response[0];
-            console.log("response", data);
-
-           
-            console.log("response", data);
-        });
+        const data = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+        };
+        Axios.post("http://127.0.0.1:6600/register", data)
+            .then((response) => {
+                console.log("good : ", response);
+            })
+            .catch((error) => {
+                console.log("error : ", error);
+            });
     };
 
     render() {
+        const { name, email, password } = this.state;
+
         return (
             <Row justify="center" style={{ marginLeft: 10, marginRight: 10 }} align="bottom">
                 <Col xl={5}>
@@ -52,77 +50,63 @@ class Register extends React.Component {
                         <h3>REGISTER</h3>
                     </center>
                     <Form name="register">
-                        <Form.Item
-                            name="name"
-                            label="Name"
-                            value={this.state.name}
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Please input your Name!",
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-                            name="email"
-                            label="E-mail"
-                            value={this.state.email}
-                            rules={[
-                                {
-                                    type: "email",
-                                    message: "The input is not valid E-mail!",
-                                },
-                                {
-                                    required: true,
-                                    message: "Please input your E-mail!",
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-                        <Form.Item
-                            name="password"
-                            label="Password"
-                            value={this.state.password}
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Please input your password!",
-                                },
-                            ]}
-                            hasFeedback
-                        >
-                            <Input.Password />
-                        </Form.Item>
-
-                        <Form.Item
-                            name="confirm"
-                            label="Confirm Password"
-                            value={this.state.password}
-                            dependencies={["password"]}
-                            hasFeedback
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Please confirm your password!",
-                                },
-                                ({ getFieldValue }) => ({
-                                    validator(rule, value) {
-                                        if (!value || getFieldValue("password") === value) {
-                                            return Promise.resolve();
-                                        }
-                                        return Promise.reject("The two passwords that you entered do not match!");
+                        <Form.Item>
+                            <Input
+                                name="name"
+                                label="Name"
+                                value={name}
+                                onChange={this.changeHandler}
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please input your Name!",
                                     },
-                                }),
-                            ]}
-                        >
-                            <Input.Password />
+                                ]}
+                            />
                         </Form.Item>
                         <Form.Item>
-                            <Button type="primary" htmlType="submit" className="register-form-button" onClick={() => this.onOk()}>
+                            <Input
+                                name="email"
+                                label="E-mail"
+                                value={email}
+                                onChange={this.changeHandler}
+                                rules={[
+                                    {
+                                        type: "email",
+                                        message: "The input is not valid E-mail!",
+                                    },
+                                    {
+                                        required: true,
+                                        message: "Please input your E-mail!",
+                                    },
+                                ]}
+                            />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Input.Password
+                                name="password"
+                                label="Password"
+                                value={password}
+                                onChange={this.changeHandler}
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please input your password!",
+                                    },
+                                ]}
+                                hasFeedback
+                            />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                className="register-form-button"
+                                onClick={() => this.submitHandler()}
+                                // onSubmit={this.submitHandler}
+                            >
                                 Register
                             </Button>
                         </Form.Item>
